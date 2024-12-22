85a7f4bcf2fb1526aefa31730b5142d1270fac9f
kenle <kle9597@gmail.com>
2024-12-21 22:53:56 -0800

    Update transaction schema to only include amount, description, and date

--- lambda/src/types/transaction.ts
+++ lambda/src/types/transaction.ts
@@ -2,10 +2,8 @@ export interface Transaction {
   userId: string;
   transactionId: string;
   amount: number;
-  description: string;
-  category: string;
+  description?: string;
   date: string;
-  type: 'income' | 'expense';
 }
 
-export interface TransactionInput extends Omit<Transaction, 'transactionId'> {} 
+export interface TransactionInput extends Omit<Transaction, 'transactionId'> { } 
